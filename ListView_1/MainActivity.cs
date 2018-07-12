7576c4be24c69ba78e63b0a2b6f5e2ee0c0fe992
Jaime Radulovich <jradulovich@hotmail.com>
2018-07-11 17:43:55 -0700

    Add project files.

---
+++ ListView_1/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Collections.Generic;
+using Android.Util;
+
+// Create a custom adapter: MyAdapter class
+// Custom Adapter works the same as SimpleListItem1 from ListView_0
+// but now can add code for changing rows for ads/purchase
+
+
+namespace ListView_1 
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity {
+        protected override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            // create data set for listview
+            var data = new List<string>();
+            for (int i = 0; i < 100; i++)
+                data.Add("Item number: " + i);
+
+            // reference listView with layout resource
+            var listView = FindViewById<ListView>(Resource.Id.listView1);
+
+			// create custom adapter and apply it to listView
+			var adapter = new MyAdapter(this, data);
+            listView.Adapter = adapter;
+
+            listView.ItemClick += ListView_ItemClick; // ListView_ItemClick added by Intellisense by pressing Tab
+        }
+
+        private void ListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e) {
+            Log.Debug("-----DEBUG-----", "Number: " + e.Position);
+        }
+    }
+}
+
---
+++ ListView_1/MyAdapter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+// Custom Adapter that works the same as SimpleListItem1 from ListView_0
+// but now can add code for changing rows for ads/purchase
+
+
+namespace ListView_1 
+{
+	public class MyAdapter : BaseAdapter<string>
+	{
+		List<string> items;  // list of strings to populate the adapter
+		Activity context;
+
+		public MyAdapter(Activity context, List<string> items) {
+			this.items = items;
+			this.context = context;
+		}
+
+
+		// Below: default methods that need to be called when the adapter is fired
+
+		public override long GetItemId(int position) { // return current adapter position
+			return position;
+		}
+
+		public override string this[int index] {  // return item in question
+			get { return items[index]; }
+		}
+
+		public override int Count {	// return the length of the list (so android knows how many rows to create)
+			get { return items.Count;  }  // .Count for List, items.Length if array
+		}
+
+		// since it's a custom adapter, we have to create a way to display our data:
+		// GetView(): create/recycle row & allow you to populate it with data
+		public override View GetView(int position, View convertView, ViewGroup parent) {
+			
+			// converView is the recycled row that scrolled off screen that we can reuse
+			View view = convertView;
+			if (view == null)   // we didn't get a recycled cell/row: have to create one
+				view = context.LayoutInflater.Inflate(Android.Resource.Layout.SimpleListItem1, null);
+
+			var item = items[position];
+
+			// this 'if' is not needed, but good away to add ad/purchase to listview (trick users into buying something :) 
+			// can also cut list from 100 items to 10 with last row asking for subscription to get the whole list :) !!!
+			if (position == 3)	// replace item 3 with below
+				item = "(Available to subscribers only)";
+
+			// inside this row we have some text elements we'd like to populate with data
+			// (Text1: default property from SimpleListItem1)
+			view.FindViewById<TextView>(Android.Resource.Id.Text1).Text = item;
+			return view;
+		}
+
+	}
+}
